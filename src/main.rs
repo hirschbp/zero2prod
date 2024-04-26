51d876fd3e5e848a7cc8054b76946b6a9066698b
Peter Hirschbeck <peter.hirschbeck@googlemail.com>
2024-04-26 18:24:00 +0200

    Added mdns service announcement.

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,57 @@
+use mdns_sd::{Error, ServiceDaemon, ServiceInfo};
+use std::net::TcpListener;
+use thiserror::Error;
 use zero2prod::run;
 
+fn unregister(mdns: &ServiceDaemon, service_fullname: &str) -> Result<(), AppError> {
+    match mdns.unregister(service_fullname) {
+        Ok(_) => Ok(()),
+        Err(err) => match err {
+            Error::Again => unregister(mdns, service_fullname),
+            e => Err(AppError::from(e)),
+        },
+    }
+}
+
+fn shutdown(mdns: ServiceDaemon) -> Result<(), AppError> {
+    match mdns.shutdown() {
+        Ok(_) => Ok(()),
+        Err(err) => match err {
+            Error::Again => shutdown(mdns),
+            e => Err(AppError::from(e)),
+        },
+    }
+}
+
 #[tokio::main]
-async fn main() -> Result<(), std::io::Error> {
-    run()?.await
+async fn main() -> Result<(), AppError> {
+    let tcp_listener = TcpListener::bind("192.168.178.34:0").expect("Failed to bind random port");
+    let port = tcp_listener.local_addr().unwrap().port();
+    let run_result = run(tcp_listener);
+
+    // Create a daemon
+    let mdns = ServiceDaemon::new().expect("Failed to create daemon");
+    let service_type = "_my-test-service._udp.local.";
+    let instance_name = "my_instance";
+
+    let my_service = ServiceInfo::new(service_type, instance_name, "test1.local.", "", port, None)?
+        .enable_addr_auto();
+
+    // Register with the daemon, which publishes the service.
+    let service_fullname = my_service.get_fullname().to_string();
+
+    mdns.register(my_service)
+        .expect("Failed to register our service");
+
+    run_result?.await.expect("Failed to launch server");
+    unregister(&mdns, &service_fullname)?;
+    shutdown(mdns)
+}
+
+#[derive(Debug, Error)]
+enum AppError {
+    #[error("MDNS error: {0}")]
+    Mdns(#[from] mdns_sd::Error),
+    #[error("Server error: {0}")]
+    Server(#[from] std::io::Error),
 }
